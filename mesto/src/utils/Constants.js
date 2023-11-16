8a8facc4f9603c5055bd127a9cc1d3f800535335
Sergei Abramenkov <exx-3@yandex.ru>
2023-11-16 16:13:38 +0300

    fix

--- mesto/src/utils/Constants.js
+++
@@ -1,9 +0,0 @@
-export const validationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  activeButtonClass: 'popup__button_type_active',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error_active'
-}
-
--- mesto/src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export default function renderLoading(submitButton) {
-  if (submitButton.textContent.indexOf(".") !== -1) {
-    submitButton.textContent = submitButton.textContent.slice(0, -3);
-  } else {
-    submitButton.textContent = submitButton.textContent + '...';
-  }
-}
